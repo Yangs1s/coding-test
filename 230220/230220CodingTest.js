1128a68dfe4dbd5cef007f266689acfd3dd8c752
SungJin Yang <sungjinahim@gmail.com>
2023-02-20 19:20:46 +0900

    230220CodingTest 추가

---
+++ 230220/230220CodingTest.js
@@ -0,0 +1,22 @@
+function solution(s, skip, index) {
+    const strArr = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const skipArr = skip.split('');
+    let ans = ''
+    const newStr = strArr.filter(e => skipArr.indexOf(e) === -1)
+
+    const newStrLength = newStr.length;
+    s.split('').map(e => {
+        let idx = newStr.indexOf(e)
+        idx += index
+        while (idx > newStrLength - 1) {
+            idx -= newStrLength
+        }
+        ans += newStr[idx]
+
+    })
+    return ans
+}
+
+solution('aukks', "wbqd", 5)
+
+console.log(solution('aukks', "wbqd", 5))
